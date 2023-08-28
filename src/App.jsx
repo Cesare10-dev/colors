3875faca7cf0963752d0ee19152a83eb12e26cf3
Cesare10-dev <cesare1087@gmail.com>
2023-08-28 22:50:27 +0200

    react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import "./App.css";
+import styled from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Palettes from "./components/Palettes";
+import Palette from "./components/Palette";
 
 function App() {
   return (
-    <main>
-      <h1>Colors</h1>
-    </main>
+    <BrowserRouter>
+      <AppStyled>
+        <div className="grid"></div>
+        <Routes>
+          <Route path="/" element={<Palettes />} />
+          <Route path="/palette/:id" element={<Palette />} />
+        </Routes>
+      </AppStyled>
+    </BrowserRouter>
   );
 }
+
+const AppStyled = styled.div`
+  min-height: 100vh;
+  background-color: slateblue;
+  position: relative;
+  .grid {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+  }
+`;
 
 export default App;
